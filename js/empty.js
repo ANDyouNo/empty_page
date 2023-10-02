bde1a85ef232d7562eab664dd7a9f5fdac353745
ANDyouNo <younofl312w@gmail.com>
2023-10-02 21:19:12 +0300

    f

--- js/empty.js
+++ js/empty.js
@@ -26,20 +26,20 @@ const stars = document.querySelectorAll('.star')
 
 const elem = document.querySelector('body'); // выбираем элемент, на котором будем отслеживать движение мыши
 
-elem.addEventListener('mousemove', function (event) {
-  // добавляем обработчик события "mousemove"
-  const x = event.clientX - (windowWidth / 2); // получаем координату X мыши
-  const y = event.clientY - (windowHeight / 2); // получаем координату Y мыши
+// elem.addEventListener('mousemove', function (event) {
+//   // добавляем обработчик события "mousemove"
+//   const x = event.clientX - (windowWidth / 2); // получаем координату X мыши
+//   const y = event.clientY - (windowHeight / 2); // получаем координату Y мыши
 
-  stars.forEach((item) => {
-    item.style.cssText += `
-    left: ${parseInt(item.dataset.x) + x / parseInt(item.dataset.coef) / 2}px; 
-    top: ${parseInt(item.dataset.y) + y / parseInt(item.dataset.coef) / 2}px;
-    `
-  })
+//   stars.forEach((item) => {
+//     item.style.cssText += `
+//     left: ${parseInt(item.dataset.x) + x / parseInt(item.dataset.coef) / 2}px; 
+//     top: ${parseInt(item.dataset.y) + y / parseInt(item.dataset.coef) / 2}px;
+//     `
+//   })
 
-  //console.log(`Координаты мыши: x=${x}, y=${y}`); // выводим координаты мыши в консоль
-});
+//   //console.log(`Координаты мыши: x=${x}, y=${y}`); // выводим координаты мыши в консоль
+// });
 
 
 const audio = new Audio('./sound/amogus.mp3')
